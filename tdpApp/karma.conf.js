5cafd151748bc3729dd4ae578949fbe7d66b289f
Robert Snelling <robert.snelling@us.pwc.com>
2015-11-09 13:08:02 -0500

    TDP-53 #comment This provides the setup structure for the client development

---
+++ tdpApp/karma.conf.js
@@ -0,0 +1,155 @@
+'use strict';
+
+var args = require('yargs').argv;
+var constants = require('./gulp_tasks/common/constants')();
+var resolutions = require('browserify-resolutions');
+var webpack = require('./webpack.config');
+var args = global.args || (process.env.ARGS ? JSON.parse(process.env.ARGS) : {});
+var moduleManager = args.bundler ? args.bundler : constants.moduleManager;
+var moduleEntry = args.module ? '/' + args.module : '';
+var isWebpack = moduleManager === 'webpack';
+
+module.exports = function(config) {
+    var debug = false;
+    try {
+        debug = JSON.parse(args._[0]).debug;
+    } catch (err) {}
+    debug = debug || args.debug;
+
+    var autowatch = true;
+    try {
+        autowatch = JSON.parse(args._[0]).autowatch;
+    } catch (err) {}
+    autowatch = autowatch || args.autowatch;
+
+    var reporters = ['mocha', 'coverage'];
+
+    var browserifyTestFiles = './client/scripts' + moduleEntry + '/**/*.test.js';
+    var webpackTestFiles = './client/scripts' + moduleEntry + '/tests.webpack.js';
+
+    var browserify = {
+        debug: true,
+        transform: [
+            ['browserify-istanbul', {
+                instrumenter: require('isparta'),
+                ignore: ['**/*.test.js', '**/*.html', '**/bower_components/**', '**/node_modules/**', '**/externals/**/*.js', '**/client/scripts/lbServices.js']
+            }],
+            ['babelify', {
+                'stage': 0,
+                'optional': ['es7.asyncFunctions'],
+                'ignore': ['./node_modules', './bower_components', './externals']
+            }]
+        ],
+        configure: function(bundle) {
+            bundle.on('prebundle', function() {
+                bundle.plugin(resolutions, '*');
+            });
+        }
+    };
+
+    webpack.cache = true;
+    webpack.devtool = 'eval'; //'inline-source-map';
+    webpack.module.preLoaders = webpack.module.preLoaders || [];
+    webpack.module.preLoaders.push({
+        test: /\.js$/,
+        exclude: /(\.webpack\.js|\.test.js|node_modules|bower_components)/,
+        //exclude: /(tests.webpack.js|.test.js|node_modules|bower_components|test)/,
+        loader: 'istanbul-instrumenter'
+    });
+
+    var preprocessors = {};
+    if (isWebpack) {
+        preprocessors[webpackTestFiles] = ['webpack', 'sourcemap'];
+    } else {
+        preprocessors[browserifyTestFiles] = ['browserify'];
+    }
+
+    if (debug === true) {
+        delete browserify.transform;
+        reporters.splice(reporters.indexOf('coverage'), 1);
+    }
+
+    config.set({
+        browserNoActivityTimeout: 120000,
+
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '',
+
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: isWebpack ? ['jasmine'] : ['browserify', 'jasmine'],
+
+        // list of files / patterns to load in the browser
+        files: [
+            //'./client/scripts/**/*.html',
+            isWebpack ? webpackTestFiles : browserifyTestFiles
+        ],
+
+        // list of files to exclude
+        exclude: [
+            './client/scripts/bundle*.js',
+            './client/scripts/main*.js'
+        ],
+
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: preprocessors,
+
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        //  reporters: ['dots', 'coverage'],
+        reporters: reporters,
+
+        // web server port
+        port: 9876,
+
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
+
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_ERROR,
+
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: autowatch,
+
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: ['PhantomJS'],
+
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: false,
+        mochaReporter: {
+            output: 'full'
+        },
+
+        coverageReporter: {
+            dir : './coverage/unit',
+            reporters: [{
+                type: 'json'
+            }, {
+                type: 'text'
+            }, {
+                type: 'text-summary'
+            }, {
+                type: 'cobertura',
+                file: 'coverage.xml'
+            }, {
+                type: 'lcov'
+            }]
+        },
+        webpack: webpack,
+        webpackMiddleware: {
+            noInfo: true,
+            stats: {
+                hash: false,
+                version: false,
+                colors: true,
+                moduleSort: 'name'
+            }
+        },
+        browserify: browserify
+    });
+};
---
+++ tdpApp/readme.md
@@ -0,0 +1,23 @@
+# tdpapp 
+[![NPM version](https://badge.fury.io/js/tdpapp.svg)](http://badge.fury.io/js/tdpapp) [![Downloads](http://img.shields.io/npm/dm/tdpapp.svg)](http://badge.fury.io/js/tdpapp)   
+[![Build Status](https://travis-ci.org//tdpapp.svg?branch=master)](https://travis-ci.org//tdpapp) [![Test Coverage](https://codeclimate.com/github//tdpapp/badges/coverage.svg)](https://codeclimate.com/github//tdpapp) [![Code Climate](https://codeclimate.com/github//tdpapp/badges/gpa.svg)](https://codeclimate.com/github//tdpapp)   
+[![Dependency Status](https://david-dm.org//tdpapp.svg)](https://david-dm.org//tdpapp) [![devDependency Status](https://david-dm.org//tdpapp/dev-status.svg)](https://david-dm.org//tdpapp#info=devDependencies) [![peerDependency Status](https://david-dm.org//tdpapp/peer-status.svg)](https://david-dm.org//tdpapp#info=peerDependencies)    
+
+
+> 
+
+[![NPM](https://nodei.co/npm/tdpapp.png?downloads=true&downloadRank=true&stars=true)](https://nodei.co/npm/tdpapp)
+
+## Usage
+
+
+## Testing
+
+
+## Changelog
+
+Recent changes can be viewed on Github on the [Releases Page](https://github.com//tdpapp/releases)
+
+## License
+
+
