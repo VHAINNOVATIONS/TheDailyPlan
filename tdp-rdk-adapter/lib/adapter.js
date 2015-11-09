bd429d9dfb4451066af33bd48ecf59595ef299e9
Afsin Ustundag <afsin.ustundag@us.pwc.com>
2015-11-09 14:47:24 -0500

    resourceDirectory and facilityList

--- tdp-rdk-adapter/lib/adapter.js
+++ tdp-rdk-adapter/lib/adapter.js
@@ -1,9 +1,58 @@
 "use strict";
 
-exports.initialize = function () {
+var util = require('util');
+var request = require('request');
 
+var baseUrl = 'https://ehmp.vaftl.us';
+
+var session = {
+    authenticate: function (userOptions, callback) {}
 };
 
-exports.authenticate = function (userInfo) {
-
+exports.newSession = function (callback) {
+    var options = {
+        method: 'GET',
+        uri: baseUrl + '/resource/resourceDirectory',
+        json: true,
+        strictSSL: false
+    };
+    request.get(options, function (err, response, body) {
+        if (err) {
+            callback(err);
+        } else {
+            var c = Object.create(session);
+            c.resourceDirectory = body;
+            var options = {
+                method: 'GET',
+                uri: baseUrl + '/resource/locations/facility-monikers',
+                json: true,
+                strictSSL: false
+            };
+            request.get(options, function (err, response, body) {
+                if (err) {
+                    callback(err);
+                } else {
+                    c.facilityMonikers = body;
+                    var options = {
+                        method: 'GET',
+                        uri: baseUrl + '/resource/authentication/list',
+                        json: true,
+                        strictSSL: false
+                    };
+                    request.get(options, function (err, response, body) {
+                        if (!err && response.statusCode !== 200) {
+                            var message = util.format('Invalid response status for %s: %s', options.uri, response.statusCode);
+                            err = new Error(message);
+                        }
+                        if (err) {
+                            callback(err);
+                        } else {
+                            c.facilityList = body;
+                            callback(null, c);
+                        }
+                    });
+                }
+            });
+        }
+    });
 };
---
+++ tdp-rdk-adapter/test/test-session.js
@@ -0,0 +1,60 @@
+"use strict";
+
+var fs = require('fs');
+var path = require('path');
+var chai = require('chai');
+var dirtyChai = require('dirty-chai');
+
+var adapter = require('../index');
+
+chai.use(dirtyChai);
+var expect = chai.expect;
+
+describe('session test', function () {
+    var generatedDir = null;
+
+    var writeDebugFile = function (filename, content) {
+
+        var filepath = path.join(generatedDir, filename);
+        if (filename.split('.')[1] === 'json') {
+            content = JSON.stringify(content, undefined, 4);
+        }
+        fs.writeFileSync(filepath, content);
+    };
+
+    before(function () {
+        generatedDir = path.join(__dirname, "generated");
+        try {
+            fs.mkdirSync(generatedDir);
+        } catch (e) {
+            if (e.code !== 'EEXIST') {
+                throw e;
+            }
+        }
+        expect(generatedDir).not.to.equal(null);
+    });
+
+    var testSession;
+    it('new session', function (done) {
+        adapter.newSession(function (err, session) {
+            if (err) {
+                done(err);
+            } else {
+                expect(session.resourceDirectory).to.exist();
+                expect(session.facilityMonikers).to.exist();
+                expect(session.facilityList).to.exist();
+
+                writeDebugFile('resourceDirectory.json', session.resourceDirectory);
+                writeDebugFile('facilityMonikers.json', session.facilityMonikers);
+                writeDebugFile('facilityList.json', session.facilityList);
+
+                testSession = session;
+                done();
+            }
+        });
+    });
+
+    it('authenticate', function () {
+        testSession.authenticate();
+    });
+});
