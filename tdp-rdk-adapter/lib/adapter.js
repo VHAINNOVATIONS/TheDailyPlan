f31ee4d677bcdf5f884112364cde914fed31aa0f
Afsin Ustundag <afsin.ustundag@us.pwc.com>
2015-11-09 15:06:08 -0500

    code cleaning

--- tdp-rdk-adapter/lib/adapter.js
+++ tdp-rdk-adapter/lib/adapter.js
@@ -1,49 +1,47 @@
 "use strict";
 
 var util = require('util');
+var _ = require('lodash');
 var request = require('request');
 
-var baseUrl = 'https://ehmp.vaftl.us';
-
 var session = {
+    baseUrl: 'https://ehmp.vaftl.us',
+    get: function (route, callback) {
+        var options = _.assign({
+            uri: this.baseUrl + route
+        }, {
+            method: 'GET',
+            json: true,
+            strictSSL: false
+        });
+        request.get(options, function (err, response, body) {
+            if ((!err) && response.statusCode !== 200) {
+                var message = util.format('Invalid response status for %s: %s', options.uri, response.statusCode);
+                err = new Error(message);
+            }
+            if (err) {
+                callback(err);
+            } else {
+                callback(null, body);
+            }
+        });
+    },
     authenticate: function (userOptions, callback) {}
 };
 
 exports.newSession = function (callback) {
-    var options = {
-        method: 'GET',
-        uri: baseUrl + '/resource/resourceDirectory',
-        json: true,
-        strictSSL: false
-    };
-    request.get(options, function (err, response, body) {
+    var c = Object.create(session);
+    c.get('/resource/resourceDirectory', function (err, body) {
         if (err) {
             callback(err);
         } else {
-            var c = Object.create(session);
             c.resourceDirectory = body;
-            var options = {
-                method: 'GET',
-                uri: baseUrl + '/resource/locations/facility-monikers',
-                json: true,
-                strictSSL: false
-            };
-            request.get(options, function (err, response, body) {
+            c.get('/resource/locations/facility-monikers', function (err, body) {
                 if (err) {
                     callback(err);
                 } else {
                     c.facilityMonikers = body;
-                    var options = {
-                        method: 'GET',
-                        uri: baseUrl + '/resource/authentication/list',
-                        json: true,
-                        strictSSL: false
-                    };
-                    request.get(options, function (err, response, body) {
-                        if (!err && response.statusCode !== 200) {
-                            var message = util.format('Invalid response status for %s: %s', options.uri, response.statusCode);
-                            err = new Error(message);
-                        }
+                    c.get('/resource/authentication/list', function (err, body) {
                         if (err) {
                             callback(err);
                         } else {
