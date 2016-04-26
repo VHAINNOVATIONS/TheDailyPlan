d5671b68fe788d01a2570e4bb6f6a74af61384cc
Afsin Ustundag <afsin.ustundag@us.pwc.com>
2016-04-25 21:44:34 -0400

    demographics, date, time hardcoded

--- tdpApp/server/pdf/index.js
+++ tdpApp/server/pdf/index.js
@@ -3,6 +3,8 @@
 var fs = require('fs');
 var path = require('path');
 
+var moment = require('moment');
+
 var PdfMake = require('pdfmake');
 
 var sections = require('./sections');
@@ -68,6 +70,19 @@ var allergies2 = {
     ]
 };
 
+var demographics = {
+    name: 'John Doe',
+    DOB: '01/12/1975',
+    location: {
+        name: 'The Cardioofy'
+    }
+};
+
+var demographics2 = {
+    name: 'Jane Doe',
+    DOB: '05/21/1985'
+};
+
 var headerText = "The Daily Plan represents current hospital activity and it is NOT a full list of everything that takes place during your hospital stay.  Your medications, treatments, appointments, etc. may change at discharge.  Please keep your personal information out of sight by storing this folder in a private place, such as a night stand drawer or bedside cabinet.";
 
 exports.generatePDF = function(patientIds, templateId) {
@@ -78,8 +93,78 @@ exports.generatePDF = function(patientIds, templateId) {
             text: headerText,
             width: '50%'
         }],
-        margin: [0, 0, 0, 20]
+        margin: [0, 0, 0, 10]
     }];
+
+    var m = moment();
+
+    var demographicsTableContent = {
+        headerRows: 0,
+        widths: ['15%', '35%', '15%', '35%'],
+        body: [
+            [{
+                text: 'Name:',
+                alignment: 'right',
+                fontSize: 18
+            }, {
+                text: demographics.name,
+                alignment: 'left',
+                bold: true,
+                fontSize: 18
+            }, {
+                text: 'Date:',
+                alignment: 'right',
+                fontSize: 18
+            }, {
+                text: m.format('MM/DD/YYYY'),
+                alignment: 'left',
+                fontSize: 18
+            }],
+            [{
+                text: 'DOB:',
+                alignment: 'right',
+                fontSize: 18
+            }, {
+                text: demographics.DOB,
+                alignment: 'left',
+                fontSize: 18
+            }, {
+                text: 'Time:',
+                alignment: 'right',
+                fontSize: 18
+            }, {
+                text: m.format('HH:mm'),
+                alignment: 'left',
+                fontSize: 18
+            }]
+        ]
+    };
+    var locationName = demographics.location && demographics.location.name;
+    if (locationName) {
+        demographicsTableContent.body.push([{
+            text: 'Location:',
+            alignment: 'right',
+            fontSize: 18
+        }, {
+            text: locationName,
+            alignment: 'left',
+            fontSize: 18
+        }, {
+            text: '',
+            alignment: 'left',
+            fontSize: 18
+        }, {
+            text: '',
+            alignment: 'left',
+            fontSize: 18
+        }]);
+    }
+
+    content.push({
+        table: demographicsTableContent,
+        margin: [5, 0, 0, 20],
+        layout: 'noBorders'
+    });
 
     for (var i=0; i<4; ++i) {
         var allergiesSection = sections.getSectionContent('Allergies', allergies)
@@ -97,6 +182,10 @@ exports.generatePDF = function(patientIds, templateId) {
         content: content,
         pageSize: 'A4',
         styles: {
+            demographics: {
+                fontSize: 18,
+                color: 'black'
+            },
             tableTitle: {
                 bold: true,
                 alignment: 'center',
