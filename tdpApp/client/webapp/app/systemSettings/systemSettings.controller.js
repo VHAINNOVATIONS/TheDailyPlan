12387c2db074820f6410415d8822c7c42f551d03
Afsin Ustundag <afsin.ustundag@us.pwc.com>
2016-05-13 17:36:30 -0400

    with list

--- tdpApp/client/webapp/app/systemSettings/systemSettings.controller.js
+++ tdpApp/client/webapp/app/systemSettings/systemSettings.controller.js
@@ -1,18 +1,33 @@
 'use strict';
 
 angular.module('tdpApp')
-    .controller('SystemSettingsCtrl', function($q, $scope, FileReader, DTOptionsBuilder, DTColumnBuilder) {
+    .controller('SystemSettingsCtrl', function($q, $scope, $compile, FileReader, DTOptionsBuilder, DTColumnBuilder) {
         var self = this;
 
+        var titleHtml = '<label for="selectchkall" style="display: none">select</label><input type="checkbox" id="selectchkall" ng-model="ctrl.selectAll" ng-click="ctrl.toggleAll(ctrl.selectAll, ctrl.selected)"> ';
         this.facilityMessages = ['Message 1 is good', 'Message 2 is good', 'Message 4 is good'];
 
-        this.clearAlerts = function () {
+        this.selectTab = function (index) {
+            this.tabIndex = index;
             this.noResults = false;
             this.displayErr.flag = false;
         };
 
+        //self.landingImage = '/common/assets/landing_images/landing' + self.landingImageIndex + '.jpg';
+
+
+        this.tabIndex = 1;
+
         this.displayErr = {};
-        this.data = [];
+        this.data = [{
+            name: 'landing1.jpg',
+        }, {
+            name: 'landing2.jpg',
+        }, {
+            name: 'landing3.jpg',
+        }, {
+            name: 'landing4.jpg',
+        }];
         this.selectedFile = null;
 
         this.newPromise = function () {
@@ -38,5 +53,21 @@ angular.module('tdpApp')
         this.addImage = function() {
             this.selectedFile = null;
         };
+
+
+        self.dtOptions = DTOptionsBuilder.fromFnPromise(function() {
+                //return $resource('data1.json').query().$promise;
+                return self.newPromise();
+            })
+            .withPaginationType('simple')
+            .withOption('dom', 'tp')
+            .withOption('ordering', false)
+            // Active Responsive plugin
+            .withOption('responsive', true);
+
+        self.dtColumns = [
+            DTColumnBuilder.newColumn('name').withTitle('Name'),
+            DTColumnBuilder.newColumn('name').withTitle('Name')
+        ];
     });
 
