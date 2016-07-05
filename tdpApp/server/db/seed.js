cb80844df56810d0b908a536a7e9ec9ee273d389
Afsin Ustundag <afsin.ustundag@us.pwc.com>
2016-07-04 22:54:04 -0400

    change health factor setting type to 8 (checkbox selection)

--- tdpApp/server/db/seed.js
+++ tdpApp/server/db/seed.js
@@ -588,7 +588,7 @@ module.exports = function(db) {
                     }),
                     db.panel_setting.create({
                         panel_type_id: pt.id,
-                        setting_type: 5,
+                        setting_type: 8,
                         setting_name: 'Include Factors'
                     }).then(function() {
                         console.log('health factor settings are created...')
