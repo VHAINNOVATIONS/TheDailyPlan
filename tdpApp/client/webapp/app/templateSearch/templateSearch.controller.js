dc2d54e896c31cbfbb9251838716ccd355659f95
Afsin Ustundag <afsin.ustundag@us.pwc.com>
2016-07-27 13:00:36 -0400

    code cleaning

--- tdpApp/client/webapp/app/templateSearch/templateSearch.controller.js
+++ tdpApp/client/webapp/app/templateSearch/templateSearch.controller.js
@@ -4,29 +4,45 @@ angular.module('tdpApp')
   .controller('TemplateSearchCtrl', function ($compile, $scope, $q, $location, DTOptionsBuilder, DTColumnBuilder, Location, $filter, Template) {
   	var self = this;
 
-    self.dtOptions = DTOptionsBuilder.fromFnPromise(function() {
-            //return $resource('data1.json').query().$promise;
-            return newPromise();
-        })
-        .withOption('createdRow', function(row, data, dataIndex) {
-            // Recompiling so we can bind Angular directive to the DT
-            $compile(angular.element(row).contents())($scope);
-        })
-        .withOption('headerCallback', function(header) {
-            if (!self.headerCompiled) {
-                // Use this headerCompiled field to only compile header once
-                self.headerCompiled = true;
-                $compile(angular.element(header).contents())($scope);
+    function newPromise() {
+        return $q( function(resolve, reject){
+            if (self.data) {
+                resolve(self.data);
+            } else {
+              resolve([]);
             }
-        })
-        .withPaginationType('full_numbers')
-        .withOption('aaSorting', [[1, 'asc']])
-        // Active Responsive plugin
-        .withOption('responsive', true);
+        });
+    }
+
+    function reloadData() {
+        var resetPaging = true;
+        self.dtInstance.reloadData(function(json) {
+          console.log(json);
+        }, resetPaging);
+    }
+
+    self.dtOptions = DTOptionsBuilder.fromFnPromise(function() {
+        return newPromise();
+    })
+    .withOption('createdRow', function(row) {
+        // Recompiling so we can bind Angular directive to the DT
+        $compile(angular.element(row).contents())($scope);
+    })
+    .withOption('headerCallback', function(header) {
+        if (!self.headerCompiled) {
+            // Use this headerCompiled field to only compile header once
+            self.headerCompiled = true;
+            $compile(angular.element(header).contents())($scope);
+        }
+    })
+    .withPaginationType('full_numbers')
+    .withOption('aaSorting', [[1, 'asc']])
+    // Active Responsive plugin
+    .withOption('responsive', true);
 
     self.dtColumns = [
         DTColumnBuilder.newColumn(null).withTitle('Select').notSortable()
-            .renderWith(function(data, type, full, meta) {
+            .renderWith(function(data, type, full) {
                 self.selected[full.id] = false;
                 return '<input type="checkbox" ng-model="ctrl.selected[' + data.id + ']" ng-click="ctrl.toggleOne(ctrl.selected)">';
             }),
@@ -51,14 +67,15 @@ angular.module('tdpApp')
     self.displayErr = {};
     self.displayErr.flag = false;
     self.errors = {};
-    var titleHtml = '<input type="checkbox" ng-model="ctrl.selectAll" ng-click="ctrl.toggleAll(ctrl.selectAll, ctrl.selected)">';
 
     //functions
     self.newPromise = newPromise;
     self.reloadData = reloadData;
-    self.clearAlerts = clearAlerts;
-    self.searchWard =searchWard;
-    self.searchClinic = searchClinic;
+
+    self.clearAlerts = function () {
+        self.noResults = false;
+        self.displayErr.flag = false;
+    };
 
     self.searchAll = function () {
       self.submitted = true;
@@ -163,136 +180,15 @@ angular.module('tdpApp')
       });
     };
 
-    self.toggleAll = toggleAll;
-    self.toggleOne = toggleOne;
-
-    self.display = function () {
-      editDeleteOrDisplay('display');
-    };
-
-    self.edit = function () {
-      editDeleteOrDisplay('edit');
-    };
-
-    self.create = function () {
-      $location.path('/layouts/create/');
-    };
-
-    self.delete = function(){
-      editDeleteOrDisplay('delete');
-    };
-
-    // Initially Populate the Wards
-    Location.getWards()
-    .then( function(data) {
-      self.wards = data;
-    })
-    .catch( function(err) {
-      self.errors.other = err.message;
-    });
-
-    // Initially Populate the Clinics
-    Location.getClinics()
-    .then( function(data) {
-      self.clinics = data;
-    })
-    .catch( function(err) {
-      self.errors.other = err.message;
-    });
-
-    function editDeleteOrDisplay(type) {
-      angular.forEach(self.selected, function(value, key) {
-        var entry = {};
-        if(value === true)
-        {
-          entry.id = key;
-          this.push(entry);
-        }
-
-      }, self.items);
-
-      switch(self.items.length) {
-        case 0:
-          self.displayErr.flag = true;
-          self.displayErr.msg = 'Please select a template to '+ type + '.';
-          break;
-        case 1:
-          //Template.setSelectedTemplates(self.items);
-          $location.path('/layouts/' + type + '/' + self.items[0].id);
-          break;
-        default:
-          self.items = [];
-          self.displayErr.flag = true;
-          self.displayErr.msg = 'Please select only one template to '+ type + '.';
-          break;
-      }
-    }
-
-    function searchClinic() {
-      self.submitted = true;
-      self.clearAlerts();
-
-      Template.byClinic(self.search.clinic)
-      .then( function(data) {
-        self.data = data;
-        self.noResults = !(data.length);
-        reloadData();
-      })
-      .catch( function(err) {
-        self.errors.other = err.message;
-      });
-    }
-
-    function searchWard() {
-      self.submitted = true;
-      self.clearAlerts();
-
-      Template.byWard(self.search.ward)
-      .then( function(data) {
-        self.data = data;
-        self.noResults = !(data.length);
-        reloadData();
-      })
-      .catch( function(err) {
-        self.errors.other = err.message;
-      });
-    };
-
-    function clearAlerts() {
-      self.noResults = false;
-      self.displayErr.flag = false;
-    }
-
-    function newPromise() {
-      return $q( function(resolve, reject){
-        if (self.data)
-        {
-          resolve(self.data);
-        }
-        else
-        {
-          resolve([]);
-        }
-      });
-    }
-
-    function reloadData() {
-        var resetPaging = true;
-        self.dtInstance.reloadData(callback, resetPaging);
-    }
-
-    function callback(json) {
-        console.log(json);
-    }
-
-    function toggleAll (selectAll, selectedItems) {
+    self.toggleAll = function (selectAll, selectedItems) {
         for (var id in selectedItems) {
             if (selectedItems.hasOwnProperty(id)) {
                 selectedItems[id] = selectAll;
             }
         }
-    }
-    function toggleOne (selectedItems) {
+    };
+
+    self.toggleOne = function (selectedItems) {
         for (var id in selectedItems) {
             if (selectedItems.hasOwnProperty(id)) {
                 if(!selectedItems[id]) {
@@ -302,6 +198,62 @@ angular.module('tdpApp')
             }
         }
         self.selectAll = true;
+    };
+
+    function editDeleteOrDisplay(type) {
+        angular.forEach(self.selected, function(value, key) {
+            var entry = {};
+            if(value === true)
+            {
+              entry.id = key;
+              this.push(entry);
+            }
+        }, self.items);
+
+        switch(self.items.length) {
+            case 0:
+                self.displayErr.flag = true;
+                self.displayErr.msg = 'Please select a template to '+ type + '.';
+                break;
+            case 1:
+                //Template.setSelectedTemplates(self.items);
+                $location.path('/layouts/' + type + '/' + self.items[0].id);
+                break;
+            default:
+                self.items = [];
+                self.displayErr.flag = true;
+                self.displayErr.msg = 'Please select only one template to '+ type + '.';
+                break;
+        }
     }
 
-  });
+    self.display = function () {
+        editDeleteOrDisplay('display');
+    };
+
+    self.edit = function () {
+        editDeleteOrDisplay('edit');
+    };
+
+    self.create = function () {
+        $location.path('/layouts/create/');
+    };
+
+    self.delete = function(){
+        editDeleteOrDisplay('delete');
+    };
+
+    // Initially Populate the Wards
+    Location.getWards().then( function(data) {
+        self.wards = data;
+    }).catch( function(err) {
+        self.errors.other = err.message;
+    });
+
+    // Initially Populate the Clinics
+    Location.getClinics().then( function(data) {
+        self.clinics = data;
+    }).catch( function(err) {
+        self.errors.other = err.message;
+    });
+});
