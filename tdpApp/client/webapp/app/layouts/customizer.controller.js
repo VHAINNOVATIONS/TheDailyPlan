db180941d20073a905417e3866a4c0ba67b3ad18
Afsin Ustundag <afsin.ustundag@us.pwc.com>
2016-07-29 18:52:39 -0400

    code cleaning

--- tdpApp/client/webapp/app/layouts/customizer.controller.js
+++ tdpApp/client/webapp/app/layouts/customizer.controller.js
@@ -36,8 +36,6 @@ angular.module('tdpApp')
 
             PanelSetting.findByPanelTypeID(panel.id)
                 .then(function(panel_settings) {
-                    $scope.actionn = 'loaded stage';
-                    $scope.settings = panel_settings;
                     var settingIdMap = panel_settings.reduce(function(r, ps) {
                         r[ps.panelSettingID] = {
                             type: ps.settingType,
@@ -93,13 +91,14 @@ angular.module('tdpApp')
                             } else {
                                 dict = false;
                             }
-                            var model8 = ps.possibleValues.map(selectedData(dict));
-                            $scope.clearAll = selectedGen(model8, false);
-                            $scope.selectAll = selectedGen(model8, true);
+                            var selectionValues = ps.possibleValues.map(selectedData(dict));
+                            $scope.clearAll = selectedGen(selectionValues, false);
+                            $scope.selectAll = selectedGen(selectionValues, true);
                             $scope.searchText = searchTextGen(ps.possibleValues);
-                            ps.selectionValues = model8;
+                            ps.selectionValues = selectionValues;
                         }
                     });
+                    $scope.settings = panel_settings;
                     $scope.loadMessage = '';
                 })
                 .catch(function(err) {
